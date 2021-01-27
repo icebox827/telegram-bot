ffafda304e6119bb4aca0e5d8e60fa211228bfae
icebox827 <icebox827@gmail.com>
2021-01-27 15:32:59 -0500

    adding scenarios to the bot file

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,12 +1,18 @@
 require 'telegram/bot'
 
-token = '1679520179:AAF3M2uEOFIB-j6sEc4A0vmpBdYuTqF0BFE'
+class NewsBot
+  attr_reader :text
 
-Telegram::Bot::Client.run(token) do |bot|
-  bot.listen do |message|
-    case message.text
-    when '/haitireportzone'
-      bot.api.send_message(chat_id: message.chat.id, text: 'Welcome to https://haitireportzone.com')
+  token = '1679520179:AAF3M2uEOFIB-j6sEc4A0vmpBdYuTqF0BFE'
+
+  Telegram::Bot::Client.run(token) do |bot|
+    bot.listen do |message|
+      case message.text
+      when '/start'
+        bot.api.send_message(chat_id: message.chat.id, text: 'Welcome to Telegram-News-Bot')
+      when '/haitireportzone'
+        bot.api.send_message(chat_id: message.chat.id, text: 'Welcome to https://haitireportzone.com')
+      end
     end
   end
 end
