89a8bf20fcbe7e8fa9f33837f14ded725d5a915b
icebox827 <icebox827@gmail.com>
2021-01-28 13:24:13 -0500

    Add test for motivation

--- spec/motivation_spec.rb
+++ spec/motivation_spec.rb
@@ -4,15 +4,15 @@ require './lib/motivation'
 describe Inspiration do
   context '#random_motivation' do
     it 'Display random motivation' do
-      news = News.new.random_news
-      expect(news).to eq news
+      motivation = Motivation.new.random_motivation
+      expect(motivation).to eq motivation
     end
   end
 
   context '#random_motivation' do
-    it 'Not display random qmotivation' do
-      news = News.new
-      expect(news).to_not eq News.new.random_news
+    it 'Not display random motivation' do
+      motivation = Motivation.new
+      expect(motivation).to_not eq Motivation.new.random_motivation
     end
   end
 end
