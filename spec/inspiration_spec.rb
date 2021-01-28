c57317678dec3c02c6987d0428991e3fca351154
icebox827 <icebox827@gmail.com>
2021-01-28 13:15:34 -0500

    Create test for Inspiration and News

--- spec/inspiration_spec.rb
+++ spec/inspiration_spec.rb
@@ -1,1 +1,18 @@
 # ./spec/inspiration_spec.rb
+require './lib/inspiration'
+
+describe Inspiration do
+  context '#random_quotes' do
+    it 'Display random quotes' do
+      news = News.new.random_news
+      expect(news).to eq news
+    end
+  end
+
+  context '#random_quotes' do
+    it 'Not display random quotes' do
+      news = News.new
+      expect(news).to_not eq News.new.random_news
+    end
+  end
+end
--- spec/news_spec.rb
+++ spec/news_spec.rb
@@ -3,14 +3,14 @@ require './lib/news'
 
 describe News do
   context '#random_news' do
-    it 'Create news' do
+    it 'Display random news' do
       news = News.new.random_news
       expect(news).to eq news
     end
   end
 
-  context '#empty_board' do
-    it 'Not create news' do
+  context '#random_news' do
+    it 'Not Display news' do
       news = News.new
       expect(news).to_not eq News.new.random_news
     end
